32cafd39efbf5ed45a17417218285cb72a3fd332
bchansen <bchansen@live.com>
2019-12-12 07:03:52 -0700

    Added dynamic content generation of title, image, and description based on search term

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 //global variables
+var momentTime = moment().format("YYYY-MM-DD");
+
 
 //Firebase login configuration
 var config = {
@@ -45,26 +47,48 @@ console.log(searchTerm);
 });
 
 $(".searchedTerm").on('click', function(){
+
+  var searchTerm = $(".searchBar").val();
 var youtubeQueryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchTerm +"&key=AIzaSyBYRddvOZlfP5RohOweM2aMGNgbByOrubc&&maxResults=1&order=viewCount";
-
+console.log(youtubeQueryURL);
 //youtube API call
-$.ajax ({
-  url:youtubeQueryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
+  $.ajax ({
+    url:youtubeQueryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
 
-  var youtubeItem = response.items[0];
-  var y_name = youtubeItem.snippet.title;
-  var y_url = "https://www.youtube.com/watch?v=" + youtubeItem.id.videoId;
-  var y_publishDate = youtubeItem.snippet.publishedAt; 
-  console.log(y_url);
-  console.log(y_publishDate);
-  console.log(y_name);
-});
+    var youtubeItem = response.items[0];
+    var y_name = youtubeItem.snippet.title;
+    var y_url = "https://www.youtube.com/watch?v=" + youtubeItem.id.videoId;
+    var ytImage = youtubeItem.snippet.thumbnails.medium.url;
+    var ytDescription = youtubeItem.snippet.description;
+    var y_publishDate = youtubeItem.snippet.publishedAt; 
+    var y_published = moment(y_publishDate).format("YYYY-MM-DD");
+    console.log(y_url);
+    console.log(y_published);
+    console.log(y_name);
+    console.log(ytDescription);
 
-var giphyApiKey = "dIxrMMRJwkqETR5DclQQXIBjt2emSBRC";
-var giphyQueryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphyApiKey + "&q=" + searchTerm + "&limit=1";
+    var ytTitle = y_name;
+    var ytURL = $("<a>").attr("href", y_url).append(ytTitle);
+    var ytImageDisplay = $("<img>").attr("src", ytImage);
+    var ytDescriptionPost = $("<p>").text(ytDescription);
+    console.log(ytImageDisplay);
+    // $(".ytLink").empty();
+    $("#ytImages").empty();
+    $("#ytTitle").empty();
+    $(".ytDescription").empty();
+
+    $("#ytTitle").append(ytTitle);
+    $("#ytImages").append(ytImageDisplay);
+    $(".ytDescription").append(ytDescriptionPost);
+
+
+  });
+
+  var giphyApiKey = "dIxrMMRJwkqETR5DclQQXIBjt2emSBRC";
+  var giphyQueryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphyApiKey + "&q=" + searchTerm + "&limit=1";
 
 //     //Giphy API call
     $.ajax ({
@@ -74,18 +98,59 @@ var giphyQueryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphyApiKe
       console.log(response);
 
       var giphyItem = response.data;
-      var g_url = giphyItem[0].images.fixed_height.url;
-      console.log(g_url);
+      var gifURL = giphyItem[0].images.fixed_height.url;
+      var gifTitle = giphyItem[0].title;
+
+      console.log(gifURL);
 
       console.log(giphyItem);
+      var gifHead = $("<a>").attr("href", gifURL).append(gifTitle);
+      var postedGifImage = $("<img>").attr("src", gifURL);
+
+      $(".giphyTitle").empty();
+      $(".giphyImage").empty();
+      $(".giphyDescription").empty();
+    
+      $(".giphyTitle").append(gifTitle);
+      $(".giphyImage").append(postedGifImage);
+
+      // $(".giphyDescription").append(newsDescription);
+
     });
 
+  //News API call
+  var currentTime = new Date();
 
-//News API call
-var currentTime = new Date();
-var momentTime = moment().format("YYYY-MM-DD");
-var newsApiKey = "0412b01d03454f5a9f798a6d22c2ea49"
-var newsQueryURL = "https://newsapi.org/v2/everything?from=" + momentTime+ "&q="+ searchTerm + "&sortBy=popularity&api_key=" + newsApiKey;
-console.log(momentTime);
-console.log(newsQueryURL);
+  var newsApiKey = "6a8e8b0ab1ed4f0f8bf80b66df812407"
+  var newsQueryURL = "https://newsapi.org/v2/everything?from=" + momentTime+ "&q="+ searchTerm + "&sortBy=popularity&apiKey=" + newsApiKey;
+  var headline = 
+  console.log(momentTime);
+  console.log(newsQueryURL);
+
+  $.ajax ({
+    url:newsQueryURL,
+    method: "GET"
+  }).then(function(response) {
+    var newsItem = response.articles;
+    var title = newsItem[0].title;
+    var description = newsItem[0].description;
+    var newsImage = newsItem[0].urlToImage;
+    console.log(title);
+    console.log(description);
+    console.log(newsImage);
+    console.log(response);
+    var postedTitle = title;
+    var newsURL = $("<a>").attr("href", response.url).append(postedTitle);
+    var postedNewsImage = $("<img>").attr("src", newsImage);
+    var newsDescription = $("<p>").text(description);
+    
+    $(".newsLink").empty();
+    $(".newsImages").empty();
+    $(".newsTitle").empty();
+
+    $(".newsTitle").append(postedTitle);
+    $(".newsImages").append(postedNewsImage);
+    $(".newsLink").append(newsDescription);
+  });
 });
+
